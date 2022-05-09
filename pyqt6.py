ddc9212044c33944931cc62756a1ca7d83a96973
SorkerLimon <sorkerlimon18@gmail.com>
2022-05-09 17:34:26 +0600

    PyQt6 coded added

---
+++ pyqt6.py
@@ -0,0 +1,10 @@
+from PyQt6.QtWidgets import QApplication,QWidget
+import sys
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle("Sorker Limon")
+window.show()
+
+app.exec()
